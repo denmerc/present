5f735644f51630545a6095fd220cd1b7a824e220
tstrimple <tstrimple@gmail.com>
2012-01-22 02:51:26 +0000

    Setup routing directory for splitting routes into multiple files

--- relyingParty.js
+++ relyingParty.js
@@ -1,19 +1,19 @@
-
 var openid = require('openid');
 
-var extensions = [ new openid.SimpleRegistration({ 'nickname' : true }) ];
+var extensions = [new openid.SimpleRegistration({
+	'nickname': true
+})];
 
-var relyingParty = new openid.RelyingParty (
-    'http://localhost:3000/verify', // Verification URL (yours)
+var relyingParty = new openid.RelyingParty('http://localhost:3000/verify', // Verification URL (yours)
     null, // Realm (optional, specifies realm for OpenID authentication)
     false, // Use stateless verification
     false, // Strict mode
     extensions); // List of extensions to enable and include
 
 exports.authenticate = function(identifier, returnUrl, callback) {
-    console.log('returnUrl: ' + returnUrl);
+	console.log('returnUrl: ' + returnUrl);
 	relyingParty.returnUrl = returnUrl;
-    relyingParty.authenticate(identifier, false, callback);
+	relyingParty.authenticate(identifier, false, callback);
 }
 
 exports.verifyAssertion = function(request, callback) {
